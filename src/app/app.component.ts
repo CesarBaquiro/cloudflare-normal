d80a2a0a8f7498656684cf55a399024d6d2ccc0d
CesarBaquiro <cmbm.kf@gmail.com>
2024-08-03 19:56:19 -0500

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'landing';
+  showAlert() {
+    Swal.fire({
+      title: '¡Hola!',
+      text: 'Esto es una alerta de SweetAlert2',
+      icon: 'info',
+      confirmButtonText: 'Aceptar',
+    });
+  }
 }
